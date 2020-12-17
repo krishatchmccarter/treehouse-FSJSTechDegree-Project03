92d636e24c2fa1621ce218b00fa3467cb3df2c99
Kris Hatch-McCarter <userkris169@yahoo.com>
2020-12-17 14:12:35 -0800

    JobRoleOtherinHTML

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Put the first field in the focus state
+const name = document.getElementById("name");
+window.onload = function() {
+name.focus();
+};
+
+//Job role section. Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
+
+//if else here
