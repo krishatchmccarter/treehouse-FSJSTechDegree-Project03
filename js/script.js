a5c8d9f57802bac21389c0aa77b4fc87f62f7ca2
Kris Hatch-McCarter <userkris169@yahoo.com>
2020-12-17 17:53:48 -0800

    completeStep4

--- js/script.js
+++ js/script.js
@@ -5,5 +5,17 @@ name.focus();
 };
 
 //Job role section. Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
+const jobRoleOtherInput = document.getElementById("other-title");
+const titleOption = document.getElementById("title");
+//set input field to hidden. Use fundtion to show when other option is selected.
+jobRoleOtherInput.style.display = 'none';
+titleOption.addEventListener('change', (e) => {
+    if (e.target.value === 'other') {
+        jobRoleOtherInput.style.display = 'block';
+    }
+    else {
+        jobRoleOtherInput.style.display = 'none';
+        }
+    }
 
-//if else here
+)
