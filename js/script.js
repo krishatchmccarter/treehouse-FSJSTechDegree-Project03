4b19863a29ee7f0ecb9721f184fcc7490394be47
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-01-01 15:49:55 -0800

    CompleteDesignEventListener

--- js/script.js
+++ js/script.js
@@ -1,21 +1,75 @@
-//Put the first field in the focus state
+//Variables
 const name = document.getElementById("name");
-window.onload = function() {
-name.focus();
-};
+const jobTitle = document.getElementById("title");
+const jobRoleInput = document.getElementById("other-title");
+const shirtColorSet= document.querySelector('#color');
+shirtDesignSet= document.querySelector('#design');
 
-//Job role section. Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
-const jobRoleOtherInput = document.getElementById("other-title");
-const titleOption = document.getElementById("title");
-//set input field to hidden. Use fundtion to show when other option is selected.
-jobRoleOtherInput.style.display = 'none';
-titleOption.addEventListener('change', (e) => {
+//Put the first field in the focus state
+    name.focus();
+
+//Add "Other" option to the Job Role section.
+//Set input field to initially hidden but will display if JavaScript is disabled.
+jobRoleInput.style.display = 'none';
+
+jobTitle.addEventListener('change', e => {
     if (e.target.value === 'other') {
-        jobRoleOtherInput.style.display = 'block';
-    }
+        jobRoleInput.style.display = 'block';
+        }
     else {
-        jobRoleOtherInput.style.display = 'none';
+        jobRoleInput.style.display = 'none';
         }
     }
-
 )
+//T-Shirt Info Section. The task is to filter the available Color options by the selected theme in the Design field.
+
+//When the form is initially loaded, update the Color field so that it's clear the user needs to select a theme before selecting a color.
+
+//Hide all color options
+for (i=0; i<shirtColorSet.length; i++){
+    shirtColorSet.options[i].hidden = true;
+}
+
+
+//Update the color field to read Please select a T-shirt theme.
+let pleaseSelectOption = document.createElement('option');
+pleaseSelectOption.textContent = "Please select a T-shirt theme";
+pleaseSelectOption.value = "Please select a T-shirt theme";
+pleaseSelectOption.selected = true;
+shirtColorSet.insertBefore(pleaseSelectOption, shirtColorSet.firstChild);
+
+//Use a change event listener on the Design menu select element to listen for changes.  Inside the event listener, use a conditional to determine what to hide, show and update.
+
+//If js puns is selected, hide the three heart js option elements in the color drop down menu, show the three js puns option elements and update the color field to the first available color.
+
+//If heart js is selected, hid the three js puns option elements in the color drop down menu, show the three heart js option elements and update the color field to the first available color.
+
+shirtDesignSet.addEventListener('change', e => {
+
+    if (e.target.value === "js puns"){
+        for (i=0; i<shirtColorSet.length; i++){
+        
+        if(shirtColorSet[i].label.includes("JS Puns shirt only")){
+            shirtColorSet.options[i].hidden = false;
+            shirtColorSet.firstChild.label = "Please select a T-shirt Color"
+        } else {
+            shirtColorSet.options[i].hidden = true;
+        }
+    }
+}
+    else {
+        for (i=0; i<shirtColorSet.length; i++){
+        
+            if(shirtColorSet[i].label.includes("JS shirt only")){
+                shirtColorSet.options[i].hidden = false;
+                shirtColorSet.firstChild.label = "Please select a T-shirt Color"
+            } else {
+                shirtColorSet.options[i].hidden = true;
+            }
+        }
+
+    }
+
+}
+)
+
